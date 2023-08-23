e1432cb1ac0e44887df23319d022d3d9c720ecfe
Jonas Greve <jonas@greve.com>
2023-08-23 20:13:46 +0200

    Create basic DataItem, add state change functionality

--- tailwind/src/App.svelte
+++ tailwind/src/App.svelte
@@ -1,37 +1,19 @@
 <script>
-  import TailwindCss from './lib/TailwindCSS.svelte';
+  import DataItem from './lib/DataItem.svelte';
+  import FrontPage from "./lib/FrontPage.svelte";
 
-  function reloadPage() {
-    location.reload();
+  let showDataItem = false;
+
+  function handleButtonClick() {
+    showDataItem = true;
   }
+
 </script>
 
-<TailwindCss/>
-
-
 <main>
-  <div class="container mx-auto bg-404258 text-dee0e7 flex flex-col justify-center items-center">
-    <div class="w-3/4 text-center flex flex-col justify-center items-center">
-      <h1 class="text-nunito font-black text-5xl mb-4">FM to EAFC Player Converter</h1>
-
-      <ul class="list-decimal list-inside w-1/3 mb-6 font-pt-sans">
-        <li class="mb-2">Placeholder Text 1</li>
-        <li class="mb-2">Placeholder Text 2</li>
-        <li class="mb-2">Placeholder Text 3</li>
-      </ul>
-
-      <button class="w-2/5 h-36 bg-474E68 border-dashed border-4 border-6B728E flex flex-col justify-center items-center cursor-pointer mb-6"
-              on:click={reloadPage}>
-        <p class="text-2xl font-pt-sans">UPLOAD</p>
-        <p class="mt-2 font-pt-sans">Click to open file explorer <br> or drag-and-drop a file</p>
-      </button>
-
-
-      <p class="w-3/5 text-dee0e7/40 text-center font-pt-sans">We recommend only using exports containing 500 players or less. There’s “technically” no limit, but your browser may freeze or crash.</p>
-    </div>
-  </div>
+  {#if !showDataItem}
+    <FrontPage on:clickButton={handleButtonClick}/>
+  {:else}
+    <DataItem/>
+  {/if}
 </main>
-
-<style>
-
-</style>
--- tailwind/src/lib/DataItem.svelte
+++ tailwind/src/lib/DataItem.svelte
@@ -1,7 +1,156 @@
 <script>
-
+    export let firstname = "";
+    export let lastname = "";
+    export let overall = 0;
+    export let birthday = "";
+    export let heightMetric = 0;
+    export let heightImperial = 0;
+    export let weightMetric = 0;
+    export let weightImperial = 0;
+    export let strongfoot = "";
+    export let weakfoot = 0;
+    export let skills = 0;
+    export let avgPace = 0;
+    export let avgShooting = 0;
+    export let avgPassing = 0;
+    export let avgDribbling = 0;
+    export let avgDefence = 0;
+    export let avgPhysical = 0;
+    export let crossing = 0;
+    export let finishing = 0;
+    export let headingAccuracy = 0;
+    export let shortPassing = 0;
+    export let volleys = 0;
+    export let dribbling = 0;
+    export let curve = 0;
+    export let freeKickAccuracy = 0;
+    export let longPassing = 0;
+    export let ballControl = 0;
+    export let acceleration = 0;
+    export let sprintSpeed = 0;
+    export let agility = 0;
+    export let reactions = 0;
+    export let balance = 0;
+    export let shotPower = 0;
+    export let jumping = 0;
+    export let stamina = 0;
+    export let strength = 0;
+    export let longShots = 0;
+    export let aggression = 0;
+    export let interceptions = 0;
+    export let positioning = 0;
+    export let vision = 0;
+    export let penalties = 0;
+    export let composure = 0;
+    export let defensiveAwareness = 0;
+    export let standingTackle = 0;
+    export let slidingTackle = 0;
+    export let GKDiving = 0;
+    export let GKHandling = 0;
+    export let GKKicking = 0;
+    export let GKPositioning = 0;
+    export let GKReflexes = 0;
 </script>
 
-<main>
+<div class="h-[80px] w-[994px] bg-[#6B728E] grid grid-cols-[70px,70px,70px,70px,70px,70px,70px,70px,70px,70px,70px,70px]
+gap-[14px] text-white font-pt-sans mb-[15px] items-center rounded">
+    <div class="col-span-4 flex flex-col items-center justify-center">
+        <!-- Place for name that spans divs 1, 2, 3, and 4 -->
+        Jude <br/> <b>Bellingham</b>
+    </div>
 
-</main>
+    <!-- div 5 -->
+    <div class="h-[80px] col-span-1 flex flex-col items-center justify-items-stretch">
+        <div class="font-bold text-center pb-[8px] pt-[3px]">
+            Overall
+        </div>
+        <div class="text-center bg-white bg-opacity-20 py-1 px-2 rounded">
+            85
+        </div>
+    </div>
+
+    <!-- div 6 -->
+    <div class="col-span-1 flex flex-col items-center justify-center text-[11px]">
+        <!-- Place for 4 lines of text in div-6 -->
+        <div class="flex justify-between w-full">
+            <span>Height:</span>
+            <span>186cm</span>
+        </div>
+
+        <div class="flex justify-between w-full">
+            <span>Weight:</span>
+            <span>75kg</span>
+        </div>
+
+        <div class="flex justify-between w-full">
+            <span>Weak Foot:</span>
+            <span>4</span>
+        </div>
+
+        <div class="flex justify-between w-full">
+            <span>Skills:</span>
+            <span>4</span>
+        </div>
+    </div>
+
+
+    <!-- div 7 -->
+    <div class="h-[80px] col-span-1 flex flex-col items-center justify-items-stretch">
+        <div class="font-bold text-center pb-[8px] pt-[3px]">
+            Pace
+        </div>
+        <div class="text-center bg-white bg-opacity-20 py-1 px-2 rounded">
+            75
+        </div>
+    </div>
+
+    <!-- div 8 -->
+    <div class="h-[80px] col-span-1 flex flex-col items-center justify-items-stretch">
+        <div class="font-bold text-center pb-[8px] pt-[3px]">
+            Shooting
+        </div>
+        <div class="text-center bg-white bg-opacity-20 py-1 px-2 rounded">
+            75
+        </div>
+    </div>
+
+    <!-- div 9 -->
+    <div class="h-[80px] col-span-1 flex flex-col items-center justify-items-stretch">
+        <div class="font-bold text-center pb-[8px] pt-[3px]">
+            Passing
+        </div>
+        <div class="text-center bg-white bg-opacity-20 py-1 px-2 rounded">
+            78
+        </div>
+    </div>
+
+    <!-- div 10 -->
+    <div class="h-[80px] col-span-1 flex flex-col items-center justify-items-stretch">
+        <div class="font-bold text-center pb-[8px] pt-[3px]">
+            Dribbling
+        </div>
+        <div class="text-center bg-white bg-opacity-20 py-1 px-2 rounded">
+            84
+        </div>
+    </div>
+
+    <!-- div 11 -->
+    <div class="h-[80px] col-span-1 flex flex-col items-center justify-items-stretch">
+        <div class="font-bold text-center pb-[8px] pt-[3px]">
+            Defence
+        </div>
+        <div class="text-center bg-white bg-opacity-20 py-1 px-2 rounded">
+            77
+        </div>
+    </div>
+
+    <!-- div 12 -->
+    <div class="h-[80px] col-span-1 flex flex-col items-center justify-items-stretch">
+        <div class="font-bold text-center pb-[8px] pt-[3px]">
+            Physical
+        </div>
+        <div class="text-center bg-white bg-opacity-20 py-1 px-2 rounded">
+            80
+        </div>
+    </div>
+</div>
