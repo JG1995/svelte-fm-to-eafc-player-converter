6f283ae7a233822845e849270c8886b46a4d5d0e
Jonas Greve <jonas@greve.com>
2023-08-28 16:33:07 +0200

    Implement file upload functionality

--- tailwind/src/App.svelte
+++ tailwind/src/App.svelte
@@ -4,6 +4,7 @@
   import FrontPage from "./lib/FrontPage.svelte";
 
   let showDataItem = false;
+  let players = [];
 
   function handleButtonClick() {
     showDataItem = true;
@@ -231,22 +232,20 @@
     }
   }
 
-  let textData = '| Rec     | Inf | Name             | Cor | Cro | Dri | Fin | Fir | Fre | Hea | Lon | L Th | Mar | Pas | Pen | Tck | Tec | Agg | Ant | Bra | Cmp | Cnt | Dec | Det | Fla | Ldr | OtB | Pos | Tea | Vis | Wor | Acc | Agi | Bal | Jum | Nat | Pac | Sta | Str | Aer | Cmd | Com | Ecc | Han | Kic | 1v1 | Pun | Ref | TRO | Thr | DoB                       | Height | Weight | Right Foot    | Left Foot     | WR | \n' +
-          '| ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------| \n' +
-          '| - -  -  |     | Lionel Messi     | 15  | 15  | 20  | 17  | 19  | 18  | 10  | 16  | 4    | 4   | 19  | 17  | 7   | 20  | 7   | 16  | 10  | 16  | 13  | 18  | 20  | 20  | 14  | 14  | 5   | 14  | 20  | 9   | 16  | 15  | 18  | 6   | 14  | 15  | 13  | 9   | 2   | 2   | 3   | 1   | 1   | 3   | 1   | 2   | 3   | 1   | 1   | 24/6/1987 (35 years old)  | 169 cm | 67 kg  | Fairly Strong | Very Strong   | 10,000 | \n' +
-          '| ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------| \n' +
-          '| - -  -  |     | Mohamed Salah    | 11  | 14  | 16  | 16  | 17  | 12  | 10  | 13  | 6    | 7   | 15  | 17  | 7   | 17  | 11  | 15  | 10  | 15  | 15  | 15  | 18  | 15  | 11  | 17  | 9   | 16  | 17  | 15  | 18  | 16  | 17  | 8   | 17  | 17  | 15  | 15  | 2   | 3   | 3   | 2   | 3   | 2   | 2   | 1   | 3   | 1   | 4   | 15/6/1992 (30 years old)  | 175 cm | 72 kg  | Weak          | Very Strong   | 8,750 | \n' +
-          '| ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------| \n' +
-          '| - -  -  |     | Antony           | 12  | 14  | 16  | 12  | 16  | 12  | 8   | 14  | 6    | 6   | 14  | 11  | 6   | 16  | 12  | 13  | 15  | 14  | 12  | 11  | 14  | 17  | 8   | 14  | 10  | 12  | 13  | 15  | 15  | 16  | 13  | 7   | 15  | 15  | 14  | 11  | 3   | 3   | 3   | 1   | 3   | 2   | 1   | 2   | 4   | 1   | 3   | 24/2/2000 (22 years old)  | 173 cm | 63 kg  | Weak          | Very Strong   | 5,500 |'
-  ; // or get it from a file input
+  function handleFileRead(event) {
+    const fileContents = event.detail;
+    // Process the file contents as required
+    players = convertPlayers(createFMPlayerObjects(fileContents));
 
-  let players = convertPlayers(createFMPlayerObjects(textData));
-
+    if(players.length > 0) {
+      showDataItem = true;
+    }
+  }
 </script>
 
 <main>
   {#if !showDataItem}
-    <FrontPage on:clickButton={handleButtonClick}/>
+    <FrontPage on:fileRead={handleFileRead}/>
   {:else}
     {#each players.slice(1) as player} <!-- Assuming each player has a unique id -->
       <CompactViewItem player={player} />
--- tailwind/src/lib/FrontPage.svelte
+++ tailwind/src/lib/FrontPage.svelte
@@ -4,10 +4,40 @@
 
     const dispatch = createEventDispatcher();
 
-    function handleClick() {
+    function handleClickDemo() {
         dispatch('clickButton');
     }
+
+    let files;
+
+    function handleClick() {
+        document.getElementById('fileInput').click();
+    }
+
+    async function handleChange() {
+        if (files && files.length > 0) {
+            const file = files[0];
+            const contents = await readFileAsString(file);
+            dispatch('fileRead', contents);
+        }
+    }
+
+    function readFileAsString(file) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = event => resolve(event.target.result);
+            reader.onerror = error => reject(error);
+            reader.readAsText(file);
+        });
+    }
+
 </script>
+
+<style>
+    #fileInput {
+        display: none;
+    }
+</style>
 
 <TailwindCss/>
 
@@ -22,13 +52,14 @@
                 <li class="mb-2">Placeholder Text 3</li>
             </ul>
 
+            <input type="file" bind:files accept=".rtf" id="fileInput" on:change={handleChange} />
             <button class="w-2/5 h-36 bg-474E68 border-dashed border-4 border-6B728E flex flex-col justify-center items-center cursor-pointer mb-6"
                     on:click={handleClick}>
                 <p class="text-2xl font-pt-sans">UPLOAD</p>
-                <p class="mt-2 font-pt-sans">Click to open file explorer <br> or drag-and-drop a file</p>
+                <p class="mt-2 font-pt-sans">Click to open file explorer <br> and choose a .RTF file</p>
             </button>
 
-            <p class="w-3/5 text-dee0e7/40 text-center font-pt-sans">We recommend only using exports containing 500 players or less. There’s “technically” no limit, but your browser may freeze or crash.</p>
+            <div class="w-3/5 text-dee0e7/40 text-center font-pt-sans">We recommend only using exports containing 500 players or less. There’s “technically” no limit, but your browser may freeze or crash.</div>
         </div>
     </div>
 </main>
